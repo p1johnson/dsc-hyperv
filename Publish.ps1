533d25a8790622a012928ee4653a862579cf0a75
Paul Johnson <pjohnson@cisilion.com>
2023-03-09 17:52:46 +0000

    Update publish.yml

--- Publish.ps1
+++ Publish.ps1
@@ -1,5 +1,5 @@
-$DscScript = '.\ConfigureJumpServer.ps1'
-$ArchivePath = '.\ConfigureJumpServer.zip'
+$DscScript = '.\ConfigureHyperVServer.ps1'
+$ArchivePath = '.\ConfigureHyperVServer.zip'
 
 Write-Host -ForegroundColor Green "Publishing DSC configuration archive $ArchivePath"
 Publish-AzVMDscConfiguration $DscScript -OutputArchivePath $ArchivePath -Force
